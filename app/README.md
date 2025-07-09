d4a921a83d60a85423318ef4918b625b75682a62
Bence Balogh <bence.balogh@datapao.com>
2025-07-09 23:27:08 +0200

    feat: add visuals to display driver and constructor standings

---
+++ app/README.md
@@ -0,0 +1,10 @@
+# App development
+
+```python
+
+databricks sync --watch . /Workspace/Users/bence.balogh@datapao.com/databricks_apps/f1-app
+
+databricks sync . /Workspace/Users/bence.balogh@datapao.com/databricks_apps/f1-app
+
+databricks apps deploy f1-app --source-code-path /Workspace/Users/bence.balogh@datapao.com/databricks_apps/f1-app
+```
--- app/app.py
+++ app/app.py
@@ -1,19 +1,30 @@
 import os
-from databricks import sql
 import pandas as pd
-import dash
-from dash import dcc, html, Input, Output, State
-import plotly.express as px
+import flask, dash
+from dash import dcc, html, Input, Output, callback, dash_table, ctx, no_update
 import dash_bootstrap_components as dbc
 import dash_ag_grid as dag
 from databricks.sdk.core import Config
-import flask  # for request context
+from databricks import sql
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print()
 
 # Ensure environment variable is set correctly
 assert os.getenv('DATABRICKS_WAREHOUSE_ID'), "DATABRICKS_WAREHOUSE_ID must be set in app.yaml."
+#assert os.getenv('ENV'), "ENV must be set in app.yaml."
+
+ENV = "dev" #os.getenv("ENV")
+
+catalog="bence-balogh-catalog"
+schema = f"f1-{ENV}" if ENV else "f1"
 
 # Databricks config
 cfg = Config()
+
 
 # Query the SQL warehouse with Service Principal credentials
 def sql_query_with_service_principal(query: str) -> pd.DataFrame:
@@ -27,6 +38,7 @@ def sql_query_with_service_principal(query: str) -> pd.DataFrame:
             cursor.execute(query)
             return cursor.fetchall_arrow().to_pandas()
 
+
 # Query the SQL warehouse with the user credentials
 def sql_query_with_user_token(query: str, user_token: str) -> pd.DataFrame:
     """Execute a SQL query and return the result as a pandas DataFrame."""
@@ -39,84 +51,90 @@ def sql_query_with_user_token(query: str, user_token: str) -> pd.DataFrame:
             cursor.execute(query)
             return cursor.fetchall_arrow().to_pandas()
 
-def load_data() -> pd.DataFrame:
+
+def load_data_with_user_token(query:str) -> pd.DataFrame:
     try:
         # Extract user access token from the request headers
         user_token = flask.request.headers.get('X-Forwarded-Access-Token')
         if not user_token:
             raise Exception("Missing access token in headers.")
-        # Query the SQL data with the user credentials
-        return sql_query_with_user_token("SELECT * FROM samples.nyctaxi.trips LIMIT 5000", user_token=user_token)
-        # In order to query with Service Principal credentials, comment the above line and uncomment the below line
-        # return sql_query_with_service_principal("SELECT * FROM samples.nyctaxi.trips LIMIT 5000")
+        return sql_query_with_user_token(query, user_token=user_token)
     except Exception as e:
         print(f"Data load failed: {str(e)}")
         return pd.DataFrame()
+    
+    
+def load_data_with_service_principal(query:str) -> pd.DataFrame:
+    try:
+        return sql_query_with_service_principal(query)
+    except Exception as e:
+        print(f"Data load failed: {str(e)}")
+        return pd.DataFrame()
+    
 
-def calculate_fare_prediction(data, pickup, dropoff):
-    """Calculate the predicted fare based on pickup and dropoff zipcodes."""
-    d = data[(data['pickup_zip'] == int(pickup)) & (data['dropoff_zip'] == int(dropoff))]
-    fare_prediction = d['fare_amount'].mean() if len(d) > 0 else 99
-    return f"Predicted Fare: ${fare_prediction:.2f}"
+def get_driver_standings(year:str) -> pd.DataFrame:
+    """Fetch driver standings data from the SQL warehouse."""
+    query = f"""
+    SELECT 
+        driver,
+        points
+    FROM `{catalog}`.`{schema}`.gold_tbl_driver_standings 
+    WHERE year = '{year}'"""
+    return load_data_with_service_principal(query)
 
-# Initialize the Dash app with Bootstrap styling
+
+def get_constructor_standings(year:str) -> pd.DataFrame:
+    """Fetch driver standings data from the SQL warehouse."""
+    query = f"""
+    SELECT 
+        constructor,
+        points
+    FROM `{catalog}`.`{schema}`.gold_tbl_constructor_standings 
+    WHERE year = '{year}'"""
+    return load_data_with_service_principal(query)
+    
+
+query = "SELECT * FROM `bence-balogh-catalog`.`f1-dev`.gold_tbl_driver_standings"
+df_driver_standings: pd.DataFrame = load_data_with_service_principal(query)
+df_years = df_driver_standings[["year"]].drop_duplicates().sort_values(by="year", ascending=False)
+years = df_years["year"].to_list()
+
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# Layout (without using global `data`)
 app.layout = dbc.Container([
     dcc.Store(id='page-load-trigger', data=0),
-    dbc.Row([dbc.Col(html.H1("Taxi Fare Distribution"), width=12)]),
     dbc.Row([
         dbc.Col([
-            dcc.Graph(id='fare-scatter', style={'height': '400px', 'width': '100%'})
-        ], width=8),
-        dbc.Col([
-            html.H3("Predict Fare"),
-            dbc.Label("From (zipcode)"),
-            dbc.Input(id='from-zipcode', type='text', value='10003'),
-            dbc.Label("To (zipcode)"),
-            dbc.Input(id='to-zipcode', type='text', value='11238'),
-            dbc.Button("Predict", id='submit-button', n_clicks=0, color='primary', className='mt-3'),
-            html.Div(id='prediction-output', className='mt-3', style={'font-size': '24px', 'font-weight': 'bold'})
-        ], width=4)
-    ]),
-    dbc.Row([
-        dbc.Col([
-            dag.AgGrid(id='data-grid')
+            dcc.Dropdown(years, years[0], id='dropdown-years'),
+            dcc.Tabs(
+                id="tabs-standings", value='tab-drivers-standings', 
+                children=[
+                    dcc.Tab(label='Drivers', value='tab-drivers-standings'),
+                    dcc.Tab(label='Standings', value='tab-constructors-standings'),
+                ]
+            ),
+            html.Div(id='tabs-standings-content')
         ], width=12)
     ])
 ], fluid=True)
 
-# Callback to load data and populate graph + grid
-@app.callback(
-    Output('fare-scatter', 'figure'),
-    Output('data-grid', 'rowData'),
-    Output('data-grid', 'columnDefs'),
-    Input('page-load-trigger', 'data')
-)
-def update_visuals(n_clicks):
-    data = load_data()
-    fig = px.scatter(
-        data,
-        x='trip_distance',
-        y='fare_amount',
-        labels={'fare_amount': 'Fare', 'trip_distance': 'Distance'}
-    )
-    return (
-        fig,
-        data.to_dict('records'),
-        [{"headerName": col, "field": col} for col in data.columns]
-    )
 
-@app.callback(
-    Output('prediction-output', 'children'),
-    Input('submit-button', 'n_clicks'),
-    State('from-zipcode', 'value'),
-    State('to-zipcode', 'value')
+@callback(
+    Output('tabs-standings-content', 'children'),
+    Input('tabs-standings', 'value'),
+    Input('dropdown-years', 'value'),
 )
-def render_prediction(n_clicks, pickup, dropoff):
-    data = load_data()
-    return calculate_fare_prediction(data, pickup, dropoff)
+def render_content(tab_value:str, year:str) -> dash_table.DataTable:
+    if not ctx.triggered_id:
+        return no_update
+    
+    if tab_value == 'tab-drivers-standings':
+        df: pd.DataFrame = get_driver_standings(year)
+        return dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
+    elif tab_value == 'tab-constructors-standings':
+        df: pd.DataFrame = get_constructor_standings(year)
+        return dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
+
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True, port=8051)
